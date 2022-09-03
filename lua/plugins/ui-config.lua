49a8fca0d400d1b8d43a78591aed745ac3358d8f
Titouan Guionneau <titouan.guionneau@epita.fr>
2022-09-03 22:34:57 +0200

    Added ensure_installed for treesitter

--- lua/plugins/ui-config.lua
+++ lua/plugins/ui-config.lua
@@ -51,11 +51,18 @@ require('lualine').setup {
 -- Treesitter
 require("nvim-treesitter.configs").setup({
     auto_install = true,
+    ensure_installed = {
+        "c", "cpp", "vim", "lua", "make", "markdown",
+        "javascript", "html", "css", "cmake", "ocaml"
+    },
+    sync_install = false,
+
     highlight = {
         enable = true,
         disable = {},
         additional_vim_regex_highlighting = false
     },
+
     rainbow = {
         enable = true,
         extended_mode = true,
