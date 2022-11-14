df5ce539790c10a8dd2f511a756d68ea72522113
Titouan Guionneau <titouan.guionneau@epita.fr>
2022-11-14 20:26:36 +0100

    fix: merge conflict

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,6 +20,17 @@ return require('packer').startup(function(use)
     }
 
     use {
+        'APZelos/blamer.nvim',
+        config = function()
+            vim.g.blamer_enabled = 1
+            vim.g.blamer_delay = 500
+            vim.g.blamer_show_in_visual_modes = 0
+            vim.g.blamer_prefix = ' '
+            vim.g.blamer_template = ' < <committer>, <committer-time> • <summary> >'
+        end
+    }
+
+    use {
         'kyazdani42/nvim-web-devicons'
     }
 
